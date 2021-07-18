a1eb7daa8414dd4719004d988e229887404eed3f
hitcracker317 <dainaso97@gmail.com>
2021-07-18 18:31:54 +0900

    envファイルにてfirebaseの環境変数を定義

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase'
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID
+} = process.env
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCMQSp5QhTLOYTeoYANJ1wVLoy7V0BqHyc",
-  authDomain: "idobatakaigi-react-ae559.firebaseapp.com",
-  databaseURL: "https://idobatakaigi-react-ae559-default-rtdb.firebaseio.com",
-  projectId: "idobatakaigi-react-ae559",
-  storageBucket: "idobatakaigi-react-ae559.appspot.com",
-  messagingSenderId: "83999182498",
-  appId: "1:83999182498:web:69d34ce331e346412bd1fb"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID
 }
 firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
