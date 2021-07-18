ba416526237278c7c9f2c356cd3db65f73c19bfa
hitcracker317 <dainaso97@gmail.com>
2021-07-18 17:24:53 +0900

    アバター画像生成にgravatarを使用

---
+++ src/components/MessageField.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TextField }from '@material-ui/core';
+
+const MessageField = () => {
+  return <TextField />
+}
+
+export default MessageField
--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import { makeStyles }from '@material-ui/core/styles'
+import { Grid, Avatar } from '@material-ui/core'
+import { gravatarPath } from '../gravatar.js'
 
-const MessageInputField = () => {
-  return <div>MessageInputField</div>
+import MessageField from './MessageField'
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+    margin: '30px'
+  }
+})
+
+const MessageInputField = ({ name }) => {
+  const classes = useStyles()
+  const avatarPath = gravatarPath(name)
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar src={avatarPath} />
+        </Grid>
+        <Grid xs={10}>
+          <MessageField />
+        </Grid>
+        <Grid xs={1}>
+          送信
+        </Grid>
+      </Grid>
+    </div>
+  )
+
 }
 export default MessageInputField
